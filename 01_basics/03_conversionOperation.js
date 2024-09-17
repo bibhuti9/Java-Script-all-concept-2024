a1e60aa2b71ade051640bdc3bf1ce6acf023a838
Balram Panda <97830953+Balram72@users.noreply.github.com>
2024-09-17 08:55:27 +0000

    Complete Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,27 +1,27 @@
-let score = "33abc"
-let score1 = null
-console.log(typeof(score));
-console.log(typeof score);
+    // let score = "33abc"
+    // let score1 = null
+    // console.log(typeof(score));
+    // console.log(typeof score);
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+    // let valueInNumber = Number(score);
+    // console.log(typeof valueInNumber);
+    // console.log(valueInNumber);
 
-let valueInNumber1 = Number(score1);
-console.log(typeof valueInNumber1);
-console.log(valueInNumber1);
+    // let valueInNumber1 = Number(score1);
+    // console.log(typeof valueInNumber1);
+    // console.log(valueInNumber1);
 
 /* "33" => 33 -> number */
 //"33abc" => NaN -> number
 // true => 1; false => 0
 
-let isLoggedIn  = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+    // let isLoggedIn  = 1
+    // let booleanIsLoggedIn = Boolean(isLoggedIn);
+    // console.log(booleanIsLoggedIn);
 
-let isLoggedIn1  = ""
-let booleanIsLoggedIn1 = Boolean(isLoggedIn1);
-console.log(booleanIsLoggedIn1);
+    // let isLoggedIn1  = ""
+    // let booleanIsLoggedIn1 = Boolean(isLoggedIn1);
+    // console.log(booleanIsLoggedIn1);
 
 // 1 => true ; 0 => false
 //"" => false
@@ -29,7 +29,49 @@ console.log(booleanIsLoggedIn1);
 
 
 
-let someNumber = 33
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+    // let someNumber = 33
+    // let stringNumber = String(someNumber);
+    // console.log(stringNumber);
+    // console.log(typeof stringNumber);
+
+
+//****************Operations*******************
+
+let value = 3
+let negValue = -value;
+console.log (negValue);  // Output = -3
+
+    // console.log(2+2)
+    // console.log(2-2)
+    // console.log(2*2)
+    // console.log(2**3)
+    // console.log(2/3)
+    // console.log(2%3)
+
+
+let str1 = "hello"
+let str2 = "Balram"
+
+console.log(str1+" "+str2);
+
+console.log("1" + 2); //OutPut = 12
+console.log(1 + "2"); //OutPut = 12
+console.log("1" + 2 + 2 ); //OutPut = 122
+console.log(1 + 2 + "2");  //OutPut = 32
+
+console.log((3+4)*5 % 3);
+console.log(+true); // OutPut = 1
+// console.log(true+); // error
+
+console.log(+"");
+
+let num1,num2, num3;
+num1 = num2 = num3 = 2+2;
+console.table([num1,num2,num3]);
+
+let gameCounter = 100;
+gameCounter ++ ;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
