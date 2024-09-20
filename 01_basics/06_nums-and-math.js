d3129dfb3b163b473b709ccf3eb62098ed45bb00
Balram Panda <97830953+Balram72@users.noreply.github.com>
2024-09-20 07:25:11 +0000

    math and num

--- 01_basics/06_nums-and-math.js
+++ 01_basics/06_nums-and-math.js
@@ -12,7 +12,24 @@
 // const otherNewNumbers = 1123.8966
 //  console.log(otherNewNumbers.toPrecision(3))//1.12e+3 
  
-const hundreds = 1000000;
- console.log(hundreds.toLocaleString('')); // 1,000,000
- console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds = 1000000;
+//  console.log(hundreds.toLocaleString('')); // 1,000,000
+//  console.log(hundreds.toLocaleString('en-IN'));
     //10,00,000
+
+//++++++++++++++++ Maths +++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2)); // top value output = 5
+// console.log(Math.floor(4.9));//lower value output = 4
+// console.log(Math.min(4,6,9,3,2,5));
+// console.log(Math.max(4,6,9,3,2,5));
+
+console.log(Math.random());//give the value 0 between  1
+console.log((Math.random()*10) + 1); // avoid the 0 value then use the plus 1
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min +1))+min)
