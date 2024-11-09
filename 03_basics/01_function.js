4f99303d524157fa6018df3e5dad8528c6a8a276
Balram Panda <97830953+Balram72@users.noreply.github.com>
2024-11-09 07:49:14 +0000

    Function and object use

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -7,29 +7,29 @@ function sayMyName(){
     console.log('M');
 }
 sayMyName //->reference
-//sayMyName(); //->execution
+ sayMyName(); //->execution
 
 function addTwoNumbers(number1,number2) /*parameters in the function*/ {
     console.log(number1 + number2);
 }
-// addTwoNumbers(3,4);   // argument in the function
-//  //output = 7
-// addTwoNumbers(3,"4");
-//   //output = 34
-// addTwoNumbers(3,"a");
-//    //output = 3a
-// addTwoNumbers(3,null);
-//     //output = 3
-// addTwoNumbers(3,true);
-//     //output = 4
-// addTwoNumbers(3,false);
-//     //output = 3
-// addTwoNumbers(3,0.1);
-//     //output = 3
+addTwoNumbers(3,4);   // argument in the function
+ //output = 7
+addTwoNumbers(3,"4");
+  //output = 34
+addTwoNumbers(3,"a");
+   //output = 3a
+addTwoNumbers(3,null);
+    //output = 3
+addTwoNumbers(3,true);
+    //output = 4
+addTwoNumbers(3,false);
+    //output = 3
+addTwoNumbers(3,0.1);
+    //output = 3
 
 
-// const result  = addTwoNumbers(3,5);  // output = 8
-// console.log("Result value:-",result); //Result value:- undefined
+const result  = addTwoNumbers(3,5);  // output = 8
+console.log("Result value:-",result); //Result value:- undefined
 
 function addTwoNumber(number3,number4) /*parameters in the function*/ {
       let results = number3 + number4
@@ -38,14 +38,14 @@ function addTwoNumber(number3,number4) /*parameters in the function*/ {
      //console.log("Balram");   // this is line code is not print
 }
 const results  = addTwoNumber(3,5);
-// console.log("Result value:-", results);  // out put = Result value:- 8
+ console.log("Result value:-", results);  // out put = Result value:- 8
 
 
 function addTwoNumberss(number5,number6) /*parameters in the function*/ {
       return number5 + number6
 }
 const resultss  = addTwoNumber(3,6);
-// console.log("Result value:-", resultss);  // out put = Result value:- 9
+  console.log("Result value:-", resultss);  // out put = Result value:- 9
 
 
 
@@ -56,12 +56,12 @@ function loginUserMessage(username){
     }
     return `${username} just logged in...`
 }
-// console.log(loginUserMessage("Balram")); // output = Balram just logged in...
-// console.log(loginUserMessage("")); // output = just logged in...
-// console.log(loginUserMessage());  //output = undefined just logged in...
+console.log(loginUserMessage("Balram")); // output = Balram just logged in...
+console.log(loginUserMessage("")); // output = just logged in...
+console.log(loginUserMessage());  //output = undefined just logged in...
 
-// loginUserMessage()  //output = Please Enter Your UserName
-// console.log(loginUserMessage());  //output = Please Enter Your UserName ,  undefined
+loginUserMessage()  //output = Please Enter Your UserName
+console.log(loginUserMessage());  //output = Please Enter Your UserName ,  undefined
 
 
 function loginUserMessages(usernames = "sam"){
@@ -72,3 +72,40 @@ function loginUserMessages(usernames = "sam"){
 }
 console.log(loginUserMessages());  // output = sam just logged in...
 console.log(loginUserMessages("Balram"));// output = Balram just logged in...
+
+
+
+function calculateCartPrice(...num1){
+   return num1
+}
+console.log(calculateCartPrice(200,300,400,500))// output =[ 200, 300, 400,800 ]
+
+function calculateCartPrices(val1,val2,...num2){
+    return num2
+ }
+ console.log(calculateCartPrices(200,300,400,500,600)) // output= [ 400, 500, 600 ]
+
+const user = {
+    username:"balram",
+    price :199
+}
+
+function handleObject(anyobject){
+    console.log(`UserName is ${anyobject.username} and Price is ${anyobject.price}`)
+}
+handleObject(user) // output = UserName is balram and Price is 199
+
+handleObject({
+    username:"Ram",
+    price:"200"
+})                  // output = UserName is Ram and Price is 200
+
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+   return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray)); // output = 400
+console.log(returnSecondValue([200,400,100,600]));// output = 400
